fa7600e62e323e22602defdab8660dcd98f0a9f0
jondercarrero126-ve <jondercarrero126@gmail.com>
2026-04-06 08:30:48 -0400

    Fotos para el Interfaz

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
+import sqlite3 
 
+class Conexion ():
+    try:
+        self.con =sqlite3.connect("banco.db")
+        self.crearTablas()
+    except Exception as ex:
+        print(ex)
+
+    def crearTablas(self
